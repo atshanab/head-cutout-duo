c9d081ed2613f9860b69462250b7e714ba013602
atshanab <atshanab@gmail.com>
2025-10-13 10:25:56 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // Auto-start camera & smarter photo saving (Web Share / iOS long-press / download)
 const CONFIG = {
-  x: parseFloat(new URLSearchParams(location.search).get('x')) || 0.4,
+  x: parseFloat(new URLSearchParams(location.search).get('x')) || 0.6,
   y: parseFloat(new URLSearchParams(location.search).get('y')) || .1,
   w: parseFloat(new URLSearchParams(location.search).get('w')) || 0.25,
   h: parseFloat(new URLSearchParams(location.search).get('h')) || 0.2,
